5ec76c3b3585981594c47a5ac3e3a5e58ebf9605
Felix Schürmann <felix.schuermann1210@googlemail.com>
2026-01-08 13:43:25 +0100

    Remove all FFI code to fix Linux segfaults - idle detection disabled

--- lib/services/idle_service.dart
+++ lib/services/idle_service.dart
@@ -1,12 +1,8 @@
-import 'dart:io';
-import 'dart:ffi';
-import 'package:ffi/ffi.dart';
 
 /// Service to detect if the user is idle (AFK) or active at the computer.
 ///
-/// Uses native APIs:
-/// - Windows: GetLastInputInfo via FFI (no external packages needed)
-/// - Linux: Disabled due to stability issues with X11/FFI
+/// Note: Currently disabled on all platforms due to FFI stability issues.
+/// Returns 'unknown' status which means the heartbeat will report 'online'.
 class IdleService {
   // Threshold in seconds after which user is considered AFK
   static const int afkThresholdSeconds = 300; // 5 minutes
@@ -14,10 +10,8 @@ class IdleService {
   /// Returns the number of seconds since the last user input (mouse/keyboard).
   /// Returns -1 if detection is not supported or fails.
   static Future<int> getIdleTimeSeconds() async {
-    if (Platform.isWindows) {
-      return _getWindowsIdleTime();
-    }
-    // Linux and other platforms: not supported
+    // Idle detection disabled due to FFI stability issues on Linux
+    // TODO: Re-enable with a stable cross-platform solution
     return -1;
   }
 
@@ -46,53 +40,6 @@ class IdleService {
     final threshold = thresholdSeconds ?? afkThresholdSeconds;
     return idleTime >= threshold ? 'idle' : 'online';
   }
-
-  // --- Windows Implementation ---
-  static int _getWindowsIdleTime() {
-    try {
-      final user32 = DynamicLibrary.open('user32.dll');
-
-      final getLastInputInfo = user32.lookupFunction<
-          Int32 Function(Pointer<_LASTINPUTINFO>),
-          int Function(Pointer<_LASTINPUTINFO>)>('GetLastInputInfo');
-
-      final kernel32 = DynamicLibrary.open('kernel32.dll');
-      final getTickCount = kernel32.lookupFunction<
-          Uint32 Function(),
-          int Function()>('GetTickCount');
-
-      final lastInputInfo = calloc<_LASTINPUTINFO>();
-      lastInputInfo.ref.cbSize = 8;
-
-      final result = getLastInputInfo(lastInputInfo);
-
-      if (result != 0) {
-        final lastInputTime = lastInputInfo.ref.dwTime;
-        final currentTime = getTickCount();
-        final idleMs = currentTime - lastInputTime;
-        final idleSeconds = idleMs ~/ 1000;
-
-        calloc.free(lastInputInfo);
-        return idleSeconds;
-      }
-
-      calloc.free(lastInputInfo);
-      return -1;
-    } catch (e) {
-      // ignore: avoid_print
-      print('IdleService: Windows idle detection error: $e');
-      return -1;
-    }
-  }
-}
-
-// --- Windows FFI Structs ---
-final class _LASTINPUTINFO extends Struct {
-  @Uint32()
-  external int cbSize;
-
-  @Uint32()
-  external int dwTime;
 }
 
 
